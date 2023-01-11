c01a862b847e90ee48ab14a995f2553e1b047dd9
Emilin Treesa Joseph <emilintresia02@gmail.com>
2023-01-11 10:11:29 +0530

    third Commit

---
+++ products.js
@@ -0,0 +1,36 @@
+export const products = [
+
+    {
+        id: 1,
+        name: 'All the Light we cannot see',
+        price: 123.21,
+        image:require('./assets/allthe.jpeg')
+
+    },
+    {
+        id: 2,
+        name: 'When Wishes Bleeds',
+        price: 89.13,
+        image:require('./assets/bleeds.jpeg')
+    },
+    {
+        id: 3,
+        name: 'Naomi s Room',
+        price: 231.14,
+        image:require('./assets/pride.jpeg')
+    },
+    {
+        id: 4,
+        name: 'Its End with US',
+        price: 67.90,
+        image:require('./assets/ends.jpeg')
+ 
+    },
+    {
+        id: 5,
+        name: 'Pride And Prejudice',
+        price: 154.03,
+        image:require('./assets/room.jpeg')
+    },
+
+]
